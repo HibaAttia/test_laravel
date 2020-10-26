771ef50b6d3d11b6ba234e3a8af89e51fd8b36fe
hiba1996-stack <hibaattia1@gmail.com>
2020-10-26 15:53:21 +0100

    modif4

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.app')
 
 @section('title','Details for' . $customer->name)
-    
+
 
 
 
@@ -10,27 +10,27 @@
     <div class="row">
         <div class="col-12">
         <h1>Details for {{ $customer->name}}</h1>
-        <p><a href="/customers/{{ $customer->id }}/edit">Edit</a></p>
+        <p><a href="/customers/{{ $customer->id }}/edit">modifier un post</a></p>
 
         <form action="/customers/{{ $customer->id }}" method="POST">
             @method('DELETE')
             @csrf
 
-            <button class="btn btn-danger" type="submit">DELETE</button>
+            <button class="btn btn-danger" type="submit">supprimer un post</button>
         </form>
         </div>
     </div>
 
     <div class="row">
         <div class="col-12">
-            <p><strong>Name</strong> {{$customer->name}}</p>
+            <p><strong>Nom</strong> {{$customer->email}}</p>
             <p><strong>Email</strong> {{$customer->email}}</p>
-            <p><strong>Company</strong> {{$customer->company->name}}</p>
+            <p><strong>Societé</strong> {{$customer->company->name}}</p>
 
         </div>
     </div>
 
-    
-    
-    
+
+
+
 @endsection
